f267b6c23d8362a3dafc987d4a75dbc81a74233b
FreedomKnight <fk@unisharp.com>
2018-02-07 15:35:57 +0800

    add categorizable

---
+++ src/Categorizable.php
@@ -0,0 +1,40 @@
+<?php
+namespace UniSharp\Category;
+
+use Cviebrock\EloquentTaggable\Taggable;
+use Illuminate\Database\Eloquent\Builder;
+
+trait Categorizable
+{
+    use Taggable;
+    
+    public function tagId($id)
+    {
+        $tags = (array)$id;
+        $this->tag($tags);
+    }
+
+    public function retagId($id)
+    {
+        $tags = (array)$id;
+        $this->retag($tags);
+    }
+
+    public function untagId($id)
+    {
+        $tags = (array)$id;
+        $this->untag($tags);
+    }
+
+    public function detagId($id)
+    {
+        $tags = (array)$id;
+        $this->detag($tags);
+    }
+
+    public function scopeWithAllTagsId(Builder $query, $id): Builder
+    {
+        $tags = (array)$id;
+        return $this->scopeWithAllTags($query, $tags);
+    }
+}
